a17067d0f28e46aad0a769d8535793210690b639
Michael Dalgaard Jepsen <mdj@systemate.dk>
2021-07-15 08:31:14 +0200

    Remove remains of queue from charge metering point created subscription

--- build/infrastructure/main/sbs-metering-point-created-sub-charges.tf
+++ build/infrastructure/main/sbs-metering-point-created-sub-charges.tf
@@ -29,28 +29,3 @@ resource "azurerm_servicebus_subscription" "sbs_metering_point_created_charges" 
   topic_name          = module.sbt_metering_point_created.name
   max_delivery_count  = 1
 }
-
-module "sbt_metering_point_created_charges" {
-  source              = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//service-bus-topic?ref=1.7.0"
-  name                = "metering-point-created-charges"
-  resource_group_name = data.azurerm_resource_group.main.name
-  namespace_name      = module.sbn_integrationevents.name
-}
-
-module "sbtar_metering_point_created_charges_listener" {
-  source              = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//service-bus-topic-auth-rule?ref=1.7.0"
-  name                = "metering-point-created-charges-listener"
-  namespace_name      = module.sbn_integrationevents.name
-  topic_name          = module.sbt_metering_point_created_charges.name
-  resource_group_name = data.azurerm_resource_group.main.name
-  listen              = true
-}
-
-module "kv_metering_point_created_charges_listener_connection_string" {
-  source              = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//key-vault-secret?ref=1.7.0"
-  name                = "metering-point-created-charges-listener-connection-string"
-  value               = trimsuffix(module.sbtar_metering_point_created_charges_listener.primary_connection_string, ";EntityPath=${module.sbt_metering_point_created.name}")
-  key_vault_id        = module.kv.id
-  tags                = data.azurerm_resource_group.main.tags
-  dependencies        = [module.kv.dependent_on, module.sbtar_metering_point_created_charges_listener.dependent_on]
-}
