0516a3d8bee0cfe73efa4e54658fa1f93e455150
René T. Nielsen <renetdev@gmail.com>
2021-09-28 13:58:57 +0200

    Update build/infrastructure/main/sbt-metering-point-created.tf
    Co-authored-by: djorgensendk <32477395+djorgensendk@users.noreply.github.com>

--- build/infrastructure/main/sbt-metering-point-created.tf
+++ build/infrastructure/main/sbt-metering-point-created.tf
@@ -17,7 +17,9 @@ module "sbt_metering_point_created" {
   name                = "metering-point-created"
   namespace_name      = module.sbn_integrationevents.name
   resource_group_name = data.azurerm_resource_group.main.name
-  dependencies        = [module.sbn_integrationevents.dependent_on]
+  dependencies        = [
+    module.sbn_integrationevents.dependent_on
+  ]
 }
 
 module "sbt_consumption_metering_point_created" {
