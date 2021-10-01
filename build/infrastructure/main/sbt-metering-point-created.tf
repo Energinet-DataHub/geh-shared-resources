c49811262c6062535ed82b70907012dacd6c1bdd
Mech0z <2316684+Mech0z@users.noreply.github.com>
2021-10-01 14:26:10 +0200

    build: add consumption mp sub

--- build/infrastructure/main/sbt-metering-point-created.tf
+++ build/infrastructure/main/sbt-metering-point-created.tf
@@ -31,3 +31,16 @@ module "sbt_consumption_metering_point_created" {
     module.sbn_integrationevents.dependent_on
   ]
 }
+
+module "sbs_consumption_metering_point_created_charge" {
+  source              = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//service-bus-subscription?ref=2.0.0"
+  name                = "consumption-metering-point-created-sub-charges"
+  resource_group_name = data.azurerm_resource_group.main.name
+  namespace_name      = module.sbn_integrationevents.name
+  topic_name          = module.sbt_consumption_metering_point_created.name
+  max_delivery_count  = 1
+  dependencies        = [
+    module.sbt_consumption_metering_point_created.dependent_on,
+    module.sbn_integrationevents.dependent_on,
+  ]
+}
