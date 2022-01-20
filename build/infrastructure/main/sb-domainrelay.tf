6a82bf1a0c45a9cf5f7c575dd425d06c056acbef
Kristian Schneider <khs@thamesconsulting.com>
2022-01-20 12:55:22 +0100

    removed zone name from module

--- build/infrastructure/main/sb-domainrelay.tf
+++ build/infrastructure/main/sb-domainrelay.tf
@@ -21,7 +21,7 @@ module "sb_domain_relay" {
   resource_group_name   = azurerm_resource_group.this.name
   location              = azurerm_resource_group.this.location
   private_endpoint_subnet_id    = module.private_endpoints_subnet.id
-  private_dns_zone_name         = "privatelink.servicebus.windows.net"
+  
   sku                   = "premium"
   auth_rules            = [
     {
