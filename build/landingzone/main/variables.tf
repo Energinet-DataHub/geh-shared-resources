323092602432382f43e3f010b03bad1c08abf9f6
Dan Stenrøjl <XDAST@energinet.dk>
2022-01-20 11:37:17 +0100

    What: Cleanup landingzone

--- build/landingzone/main/variables.tf
+++ build/landingzone/main/variables.tf
@@ -40,46 +40,6 @@ variable project_name {
   type          = string
 }
 
-variable apim_publisher_email {
-  type          = string
-  description   = "(Required) The email of publisher/company."
-}
-
-variable apimao_charges_domain_ingestion_function_url {
-  type          = string
-  description   = "Url of the Charges domain ingestion function, used for configuration inside the API Managment API."
-}
-
-variable apimao_market_roles_domain_ingestion_function_url {
-  type          = string
-  description   = "Url of the Market Roles domain ingestion function, used for configuration inside the API Managment API."
-}
-
-variable apimao_messagehub_domain_ingestion_function_url {
-  type          = string
-  description   = "Url of the Message hub domain ingestion function, used for configuration inside the API Managment API."
-}
-
-variable apimao_metering_point_domain_ingestion_function_url {
-  type          = string
-  description   = "Url of the Metering Point domain ingestion function, used for configuration inside the API Managment API."
-}
-
-variable apimao_timeseries_domain_ingestion_function_url {
-  type          = string
-  description   = "Url of the Timeseries domain ingestion function, used for configuration inside the API Managment API."
-}
-
-variable apim_b2c_tenant_id {
-  type          = string
-  description   = "ID of the B2C tenant hosting the backend app registrations authorizing against."
-}
-
-variable backend_service_app_id {
-  type          = string
-  description   = "The app/client ID of the backend app registration."
-}
-
 variable vm_user_name {
   type          = string
   description   = "Deployment Agent VM user name for SSH connection."
@@ -93,9 +53,4 @@ variable github_runner_token {
 variable github_runner_name {
   type          = string
   description   = "Name of GitHub self-hosted runner."
-}
-
-variable azurerm_virtual_network_name {
-  type          = string
-  description   = "Name of the virtual network"
 }
