74a5433a3db191306b003bf2bd692f544cfbe991
Daniel Bjerring Jørgensen <dbj1986dk@gmail.com>
2022-02-01 15:30:34 +0100

    remove vm_user_name - came in during merge

--- build/infrastructure/main/variables.tf
+++ build/infrastructure/main/variables.tf
@@ -90,11 +90,6 @@ variable frontend_service_app_id {
   description   = "The app/client ID of the frontend app registration."
 }
 
-variable vm_user_name {
-  type          = string
-  description   = "Name of the private dns resource group"
-}
-
 variable private_dns_resource_group_name {
   type          = string
   description   = "Name of the private dns resource group"
