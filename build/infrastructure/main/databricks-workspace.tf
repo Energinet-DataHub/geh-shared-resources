5890086383d70211d479e4d91bebf5411f28402e
Kristian Schneider <khs@thamesconsulting.com>
2022-02-10 09:46:29 +0100

    create and set token  in KV

--- build/infrastructure/main/databricks-workspace.tf
+++ build/infrastructure/main/databricks-workspace.tf
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-resource "azurerm_databricks_workspace" "dbw_aggregations" {
+resource "azurerm_databricks_workspace" "dbw_shared" {
   name                = "dbw-${lower(var.domain_name_short)}-${lower(var.environment_short)}-${lower(var.environment_instance)}"
   resource_group_name = azurerm_resource_group.this.name
   location            = azurerm_resource_group.this.location
@@ -24,7 +24,7 @@ module "kvs_databricks_workspace_id" {
   source        = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/key-vault-secret?ref=5.1.0"
 
   name          = "dbw-shared-workspace-id"
-  value         = azurerm_databricks_workspace.dbw_aggregations.workspace_id
+  value         = azurerm_databricks_workspace.dbw.workspace_id
   key_vault_id  = module.kv_shared.id
 
   tags          = azurerm_resource_group.this.tags
@@ -34,8 +34,41 @@ module "kvs_databricks_workspace_url" {
   source        = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/key-vault-secret?ref=5.1.0"
 
   name          = "dbw-shared-workspace-url"
-  value         = azurerm_databricks_workspace.dbw_aggregations.workspace_url
+  value         = azurerm_databricks_workspace.dbw.workspace_url
   key_vault_id  = module.kv_shared.id
 
   tags          = azurerm_resource_group.this.tags
+}
+
+resource "azurerm_key_vault_access_policy" "this" {
+  key_vault_id = module.kv_shared.id
+
+  tenant_id = module.kv_shared.tenant_id
+  object_id = data.azurerm_client_config.current.object_id
+
+  secret_permissions = [
+    "set",
+  ]
+}
+
+resource "null_resource" "databricks_token" {
+  triggers = {
+    workspace = azurerm_databricks_workspace.dbw.id
+    key_vault_access = azurerm_key_vault_access_policy.this.id
+  }
+  provisioner "local-exec" {
+    command = "${path.module}/scripts/generate-pat-token.sh"
+    environment = {
+      RESOURCE_GROUP = var.resource_group_name
+      DATABRICKS_WORKSPACE_RESOURCE_ID = azurerm_databricks_workspace.databricks.id
+      KEY_VAULT = azurerm_key_vault.databricks_token.name
+      SECRET_NAME = "DATABRICKS-TOKEN"
+      DATABRICKS_ENDPOINT = "https://${data.azurerm_resources.databricks.resources[0].location}.azuredatabricks.net"
+      # ARM_CLIENT_ID, ARM_CLIENT_SECRET, ARM_TENANT_ID are already 
+      # present in the environment if you are using the Terraform
+      # extension for Azure DevOps or the starter from 
+      # https://github.com/algattik/terraform-azure-pipelines-starter.
+      # Otherwise, provide them as additional variables.
+    }
+  }
 }
