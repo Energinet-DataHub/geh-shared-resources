83c2e9ba3e8e5829d7c9e309c2d37935ca3f2038
Jens Riis <jens.j.riis@gmail.com>
2022-02-16 15:54:44 +0100

    Use log analytics workspace in app-insights

--- build/infrastructure/main/appi-shared.tf
+++ build/infrastructure/main/appi-shared.tf
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 module "appi_shared" {
-  source                = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/application-insights?ref=5.5.0"
+  source                = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/application-insights?ref=5.6.0"
 
   name                         = "shared"
   project_name                 = var.domain_name_short
@@ -20,7 +20,7 @@ module "appi_shared" {
   environment_instance         = var.environment_instance
   resource_group_name          = azurerm_resource_group.this.name
   location                     = azurerm_resource_group.this.location
-  log_analytics_workspace_name = azurerm_log_analytics_workspace.log_workspace_shared.id 
+  log_analytics_workspace_id   = azurerm_log_analytics_workspace.log_workspace_shared.id 
   tags                         = azurerm_resource_group.this.tags
 }
 
