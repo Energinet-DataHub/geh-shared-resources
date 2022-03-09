2feeda725ca657d04672050b7378972b4eaf34cf
Daniel Bjerring Jørgensen <dbj1986dk@gmail.com>
2022-03-09 12:53:04 +0100

    fix tf - update secret names

--- build/infrastructure/main/databricks-workspace.tf
+++ build/infrastructure/main/databricks-workspace.tf
@@ -21,9 +21,9 @@ module "dbw_shared" {
   resource_group_name                       = azurerm_resource_group.this.name
   location                                  = azurerm_resource_group.this.location
   sku                                       = "standard"
-  main_virtual_network_id                   = data.azurerm_key_vault_secret.vnet_shared_id.value
-  main_virtual_network_name                 = data.azurerm_key_vault_secret.vnet_shared_name.value
-  main_virtual_network_resource_group_name  = data.azurerm_key_vault_secret.vnet_shared_resource_group.value
+  main_virtual_network_id                   = data.azurerm_key_vault_secret.vnet_id.value
+  main_virtual_network_name                 = data.azurerm_key_vault_secret.vnet_name.value
+  main_virtual_network_resource_group_name  = data.azurerm_key_vault_secret.vnet_resource_group.value
   databricks_virtual_network_address_space  = "10.142.92.0/23"
   private_subnet_address_prefix             = "10.142.92.1/24"
   public_subnet_address_prefix              = "10.142.93.1/24"
@@ -32,7 +32,7 @@ module "dbw_shared" {
 }
 
 module "kvs_databricks_workspace_id" {
-  source        = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/key-vault-secret?ref=5.1.0"
+  source        = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/key-vault-secret?ref=6.0.0"
 
   name          = "dbw-shared-workspace-id"
   value         = module.dbw_shared.id
@@ -42,7 +42,7 @@ module "kvs_databricks_workspace_id" {
 }
 
 module "kvs_databricks_workspace_url" {
-  source        = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/key-vault-secret?ref=5.1.0"
+  source        = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/key-vault-secret?ref=6.0.0"
 
   name          = "dbw-shared-workspace-url"
   value         = module.dbw_shared.workspace_url
