2e19cde6eb0f85b1ce3d55826be86693a0aabf0a
Daniel Bjerring Jørgensen <dbj1986dk@gmail.com>
2022-03-14 15:20:25 +0100

    downgrade appi to 5.6.0

--- build/infrastructure/main/appi-shared.tf
+++ build/infrastructure/main/appi-shared.tf
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 module "appi_shared" {
-  source                = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/application-insights?ref=6.0.0"
+  source                = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/application-insights?ref=5.6.0"
 
   name                         = "shared"
   project_name                 = var.domain_name_short
