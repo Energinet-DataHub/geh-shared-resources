ac8fd3aa9bb1382a5b5e47b5946a4550017adf3a
René Nielsen <renetdev@gmail.com>
2022-04-06 18:53:40 +0200

    fix async

--- .github/actions/cleanup-workflows/index.js
+++ .github/actions/cleanup-workflows/index.js
@@ -23,7 +23,7 @@ const main = async () => {
     }
 
     if (excluded_workflows) {
-      console.log('Skipping excluded workflows: ', excluded_workflows);
+      console.log('Skipping excluded workflows:', excluded_workflows);
     }
 
     // Since github variable github.repository is in the format {owner}/{repository}
@@ -39,62 +39,106 @@ const main = async () => {
     const rateLimitResponse = await octokit.request('GET /rate_limit');
     githubApiCallsUsed = rateLimitResponse.data.rate.used;
     if (githubApiCallsUsed >= githubApiCallsThrottleCap){
-      throw new Error('Close to spending up throttles for the current hour, please try again later.');
+      throw new Error('Almost spent up all requests for this hour, aborting - please try again later.');
     }
     
-    const workflowsResponse = await octokit.rest.actions.listRepoWorkflows({
-      owner,
-      repo,
-    });
-    githubApiCallsUsed++;
+    const workflowsResponse = await octokit
+      .rest
+      .actions
+      .listRepoWorkflows({
+        owner,
+        repo,
+      })
+      .then(githubApiCallsUsed++);
 
     if (workflowsResponse.data.workflows){
       // Iterating through workflows
       for (let workflowIndex = 0; workflowIndex < workflowsResponse.data.workflows.length; workflowIndex++) {
         const workflow = workflowsResponse.data.workflows[workflowIndex];
-        const pathWithoutPrefix = workflow.path.replace('.github/workflows/', '');
-
-        // Only handling non excluded workflows
-        if (excluded_workflows && !excluded_workflows.includes(pathWithoutPrefix)) {
-          const workflowRuns = await octokit.rest.actions.listWorkflowRuns({
-            owner,
-            repo,
-            workflow_id: workflow.id,
-          });
-          
-          githubApiCallsUsed++;
-
-          if (githubApiCallsUsed >= githubApiCallsThrottleCap){
-            throw new Error('Close to spending up throttles for the current hour, please try again later.');
-          }
-
-          if (workflowRuns.data.workflow_runs) {
-            console.log(`Cleaning runs of workflow ${workflow.name}`);
-
-            for (let workflowRunIndex = 0; workflowRunIndex < workflowsResponse.data.workflows.length; workflowRunIndex++) {
-              const workflowRun = workflowsResponse.data.workflows[workflowRunIndex];
-              await octokit.rest.actions.deleteWorkflowRun({
-                owner,
-                repo,
-                run_id: workflowRun.id,
-              });
-              githubApiCallsUsed++;
-              if (githubApiCallsUsed >= githubApiCallsThrottleCap){
-                throw new Error('Close to spending up throttles for the current hour, please try again later.');
-              }
-
-              console.log(`Run ${workflowRun.id} deleted`);
-            };
-          }
-        }
+        await cleanWorkflow(octokit, owner, repo, excluded_workflows, workflow);
       };
     }
   } catch (error) {
-    console.log(error.message);
+    console.log(error);
     core.setFailed(error.message);
   }
 
   console.log('Workflow cleanup done');
+}
+
+const cleanWorkflow = async(octokit, owner, repo, excluded_workflows, workflow) => {
+  const pathWithoutPrefix = workflow.path.replace('.github/workflows/', '');
+  console.log(`Initiating cleaning of workflow: ${pathWithoutPrefix}`);
+  let workflowCleared = false;
+
+  if (excluded_workflows && excluded_workflows.includes(pathWithoutPrefix)) {
+    console.log(`Workflow ${pathWithoutPrefix} excluded, continuing`);
+    return;
+  }
+  else {
+    // Github only returns max 100 per request, so we need to keep iterating until runs equals 0
+    while (!workflowCleared) {
+      console.log(`Workflow ${pathWithoutPrefix} not excluded, getting workflow runs`);
+
+      try {
+        // Only handling non excluded workflows
+        const workflowRuns = await octokit
+          .rest
+          .actions
+          .listWorkflowRuns({
+            owner,
+            repo,
+            workflow_id: workflow.id,
+          })
+          .then(githubApiCallsUsed++);
+
+        if (githubApiCallsUsed >= githubApiCallsThrottleCap){
+          throw new Error('Almost spent up all requests for this hour, aborting - please try again later.');
+        }
+
+        if (workflowRuns.data.workflow_runs.length <= 0){
+          console.log('Workflow cleared');
+          workflowCleared = true;
+          break;
+        }
+
+        if (workflowRuns.data.workflow_runs) {
+          console.log(`Cleaning runs of workflow ${workflow.name}`);
+
+          await cleanWorkflowRuns(octokit, owner, repo, workflowRuns.data.workflow_runs);
+        }
+      }
+      catch {
+        // When the workflow is cleared, the call will fail with 404.
+        // Since the workflow is completely removed
+        break;
+      }
+    }
+  }
+}
+
+const cleanWorkflowRuns = async(octokit, owner, repo, workflows) => {
+  for (let index = 0; index < workflows.length; index++) {
+    const workflowRun = workflows[index];
+
+    console.log(`Deleting run with id: ${workflowRun.id}`);
+
+    await octokit
+      .rest
+      .actions
+      .deleteWorkflowRun({
+        owner,
+        repo,
+        run_id: workflowRun.id,
+      })
+      .then(githubApiCallsUsed++);
+
+    if (githubApiCallsUsed >= githubApiCallsThrottleCap){
+      throw new Error('Almost spent up all requests for this hour, aborting - please try again later.');
+    }
+
+    console.log(`Succesfully deleted run with id: ${workflowRun.id}`);
+  };
 }
 
 // Call the main function to run the action
