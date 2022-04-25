7e199e9665deb950a97d169e45a400f85b7d61d6
René Nielsen <renetdev@gmail.com>
2022-04-25 12:11:35 +0200

    added elastic pooling to SQL server

--- build/infrastructure/main/mssql-data.tf
+++ build/infrastructure/main/mssql-data.tf
@@ -16,7 +16,7 @@ locals {
 }
 
 module "mssql_data" {
-  source                        = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/mssql-server?ref=5.7.0"
+  source                        = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/mssql-server?ref=renetnielsen/elastic-pooling"
 
   name                          = "data"
   project_name                  = var.domain_name_short
