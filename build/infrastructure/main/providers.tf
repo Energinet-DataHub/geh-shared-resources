fb9dfa87db0a835637c9d042cd9d32f5433f4812
Daniel Bjerring Jørgensen <dbj1986dk@gmail.com>
2022-04-27 10:27:27 +0200

    update version of azurerum provider

--- build/infrastructure/main/providers.tf
+++ build/infrastructure/main/providers.tf
@@ -16,7 +16,7 @@ terraform {
 
   required_providers {
     # It is recommended to pin to a given version of the Azure provider
-    azurerm = "=2.98.0"
+    azurerm = "=3.3.0"
 	  null = "~> 2.1"
   }
 }
