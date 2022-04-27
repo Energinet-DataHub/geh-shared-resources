073950bb26771e63ec863f56da8f6f9cecd7b534
Daniel Bjerring Jørgensen <dbj1986dk@gmail.com>
2022-04-27 10:31:42 +0200

    use old resource for dashboard (prior azurerm 3.x)

--- build/infrastructure/main/providers.tf
+++ build/infrastructure/main/providers.tf
@@ -16,7 +16,7 @@ terraform {
 
   required_providers {
     # It is recommended to pin to a given version of the Azure provider
-    azurerm = "=3.3.0"
+    azurerm = "=2.97.0"
 	  null = "~> 2.1"
   }
 }
