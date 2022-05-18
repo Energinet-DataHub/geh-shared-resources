928ceb4e8e83664211a775b9a1edbb77f171f313
René Nielsen <renetdev@gmail.com>
2022-05-18 12:54:12 +0200

    tf example

--- build/infrastructure/main/scripts/generate-pat-token.sh
+++ build/infrastructure/main/scripts/generate-pat-token.sh
@@ -1,47 +1,18 @@
 #!/bin/bash
 
-# Bash strict mode, stop on any error
+# Exit if any of the intermediate steps fail
 set -e
 
-# Ensure all required environment variables are present
-# test -n "$DATABRICKS_WORKSPACE_RESOURCE_ID"
-# test -n "$ARM_CLIENT_ID"
-# test -n "$ARM_CLIENT_SECRET"
-# test -n "$ARM_TENANT_ID"
-# test -n "$DATABRICKS_ENDPOINT"
+# Extract "foo" and "baz" arguments from the input into
+# FOO and BAZ shell variables.
+# jq will ensure that the values are properly quoted
+# and escaped for consumption by the shell.
+eval "$(jq -r '@sh "FOO=\(.foo) BAZ=\(.baz)"')"
 
-echo "$DATABRICKS_WORKSPACE_RESOURCE_ID"
-echo "$ARM_CLIENT_ID"
-echo "$ARM_CLIENT_SECRET"
-echo "$ARM_TENANT_ID"
-echo "$DATABRICKS_ENDPOINT"
+# Placeholder for whatever data-fetching logic your script implements
+FOOBAZ="$FOO $BAZ"
 
-pat_token="test"
-jq -n --arg token "$pat_token" '{"token":$token}'
-
-# # Login
-# az login --service-principal -u "$ARM_CLIENT_ID" -p "$ARM_CLIENT_SECRET" -t "$ARM_TENANT_ID"
-
-
-# # Get a token for the global Databricks application.
-# # The resource name is fixed and never changes.
-# token_response=$(az account get-access-token --resource 2ff814a6-3304-4ab8-85cb-cd0e6f879c1d)
-# token=$(jq .accessToken -r <<< "$token_response")
-
-# # Get a token for the Azure management API
-# token_response=$(az account get-access-token --resource https://management.core.windows.net/)
-# azToken=$(jq .accessToken -r <<< "$token_response")
-
-# # Generate a PAT token. Note the quota limit of 600 tokens.
-# api_response=$(curl -sf $DATABRICKS_ENDPOINT/api/2.0/token/create \
-#   -H "Authorization: Bearer $token" \
-#   -H "X-Databricks-Azure-SP-Management-Token:$azToken" \
-#   -H "X-Databricks-Azure-Workspace-Resource-Id:$DATABRICKS_WORKSPACE_RESOURCE_ID" \
-#   -d '{ "comment": "Terraform-generated token" }')
-# pat_token=$(jq .token_value -r <<< "$api_response")
-
-# # jq -n --arg token "$pat_token" '{"token":$token}'
-
-# # az keyvault secret set --vault-name "$KEY_VAULT" -n "$SECRET_NAME" --value "$pat_token"
-
-# az logout
+# Safely produce a JSON object containing the result value.
+# jq will ensure that the value is properly quoted
+# and escaped to produce a valid JSON string.
+jq -n --arg foobaz "$FOOBAZ" '{"foobaz":$foobaz}'
