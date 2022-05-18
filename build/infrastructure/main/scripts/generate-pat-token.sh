ee233f53e10d059ffe82f0674ff1c8f746fa9304
Daniel Bjerring Jørgensen <dbj1986dk@gmail.com>
2022-05-18 14:14:22 +0200

    debug 1: move after az login

--- build/infrastructure/main/scripts/generate-pat-token.sh
+++ build/infrastructure/main/scripts/generate-pat-token.sh
@@ -8,22 +8,22 @@ eval "$(jq -r '@sh "DATABRICKS_WORKSPACE_RESOURCE_ID=\(.DATABRICKS_WORKSPACE_RES
 # Login
 az login --service-principal -u "$ARM_CLIENT_ID" -p "$ARM_CLIENT_SECRET" -t "$ARM_TENANT_ID"
 
-# Get a token for the global Databricks application.
-# The resource name is fixed and never changes.
-token_response=$(az account get-access-token --resource 2ff814a6-3304-4ab8-85cb-cd0e6f879c1d)
-token=$(jq .accessToken -r <<< "$token_response")
-
-# Get a token for the Azure management API
-token_response=$(az account get-access-token --resource https://management.core.windows.net/)
-azToken=$(jq .accessToken -r <<< "$token_response")
-
-# Generate a PAT token. Note the quota limit of 600 tokens.
-api_response=$(curl -sf $DATABRICKS_ENDPOINT/api/2.0/token/create \
-  -H "Authorization: Bearer $token" \
-  -H "X-Databricks-Azure-SP-Management-Token:$azToken" \
-  -H "X-Databricks-Azure-Workspace-Resource-Id:$DATABRICKS_WORKSPACE_RESOURCE_ID" \
-  -d '{ "comment": "Terraform-generated token" }')
-pat_token=$(jq .token_value -r <<< "$api_response")
-
 new_token="test123456789"
 jq -n --arg token "$new_token" '{"token":$token}'
+
+# # Get a token for the global Databricks application.
+# # The resource name is fixed and never changes.
+# token_response=$(az account get-access-token --resource 2ff814a6-3304-4ab8-85cb-cd0e6f879c1d)
+# token=$(jq .accessToken -r <<< "$token_response")
+
+# # Get a token for the Azure management API
+# token_response=$(az account get-access-token --resource https://management.core.windows.net/)
+# azToken=$(jq .accessToken -r <<< "$token_response")
+
+# # Generate a PAT token. Note the quota limit of 600 tokens.
+# api_response=$(curl -sf $DATABRICKS_ENDPOINT/api/2.0/token/create \
+#   -H "Authorization: Bearer $token" \
+#   -H "X-Databricks-Azure-SP-Management-Token:$azToken" \
+#   -H "X-Databricks-Azure-Workspace-Resource-Id:$DATABRICKS_WORKSPACE_RESOURCE_ID" \
+#   -d '{ "comment": "Terraform-generated token" }')
+# pat_token=$(jq .token_value -r <<< "$api_response")
