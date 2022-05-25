c655f5494fdb79e4a9b82329dd54fa7f2113a7a5
Chris Brohus <CBS@energinet.dk>
2022-05-25 11:16:13 +0200

    Added new service bus topic and subscription for "Consumer moved in"

---
+++ build/infrastructure/main/sbt-market-roles-consumer-moved-in.tf
@@ -0,0 +1,46 @@
+﻿# Copyright 2020 Energinet DataHub A/S
+#
+# Licensed under the Apache License, Version 2.0 (the "License2");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+module "sbt_consumer_moved_in" {
+    source = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/service-bus-topic?ref=6.0.0"
+    
+    name = "consumer-moved-in"
+    namespace_id = module.sb_domain_relay.id
+    subscriptions = [
+        {
+            name = "consumer-moved-in"
+            max_delivery_count = 10
+        }
+    ]
+}
+
+module "kvs_sbt_consumer_moved_in_name" {
+    source          = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/key-vault-secret?ref=6.0.0"
+    
+    name            = "sbt-consumer-moved-in-name"
+    value           = module.sbt_consumer_moved_in.name
+    key_vault_id    = module.kv_shared.id
+    
+    tags            = azurerm_resource_group.this.tags
+}
+
+module "kvs_sbs_consumer_moved_in_name" {
+    source          = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/key-vault-secret?ref=6.0.0"
+    
+    name            = "sbs-consumer-moved-in-name"
+    value           = "consumer-moved-in"
+    key_vault_id    = "module.kv_shared.id"
+    
+    tags            = azurerm_resource_group.this.tags
+}
