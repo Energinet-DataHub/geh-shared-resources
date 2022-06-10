932d50b231df9c60429138aef73403caeb9c84b1
René Nielsen <renetdev@gmail.com>
2022-06-10 09:05:53 +0200

    add TF versiong

--- build/infrastructure/main/plan-shared.tf
+++ build/infrastructure/main/plan-shared.tf
@@ -11,27 +11,31 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-module "plan_services" {
-  source              = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/app-service-plan?ref=7.0.0"
+module "plan_shared" {
+  source              = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/app-service-plan?ref=6.0.0"
 
-  name                           = "services"
+  name                           = "shared"
   project_name                   = var.domain_name_short
   environment_short              = var.environment_short
   environment_instance           = var.environment_instance
   resource_group_name            = azurerm_resource_group.this.name
   location                       = azurerm_resource_group.this.location
-  os_type                        = "Windows"
-  sku_name                       = "P1v3"
+  kind                           = "Windows"
   monitor_alerts_action_group_id = module.ag_primary.id
+
+  sku                            = {
+    tier  = "PremiumV3"
+    size  = "P1v3"
+  }
 
   tags                           = azurerm_resource_group.this.tags
 }
 
-module "kvs_plan_services_id" {
+module "kvs_plan_shared_id" {
   source        = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/key-vault-secret?ref=7.0.0"
 
   name          = "plan-shared-id"
-  value         = module.plan_services.id
+  value         = module.plan_shared.id
   key_vault_id  = module.kv_shared.id
 
   tags          = azurerm_resource_group.this.tags
