b8b7f1f5b5e0d26db1605ccfc29ab660924fd52c
Daniel Bjerring Jørgensen <dbj1986dk@gmail.com>
2022-08-02 12:02:35 +0200

    order changed

--- build/infrastructure/env/p-001/kvs-data-migrationn.tf
+++ build/infrastructure/env/p-001/kvs-data-migrationn.tf
@@ -11,31 +11,11 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-module "kvs_sas_token_datamig_wholesale" {
-  source        = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/key-vault-secret?ref=7.0.0"
-
-  name          = "datamig-wholesale"
-  value         = var.sas_token_datamig_wholesale
-  key_vault_id  = module.kv_shared.id
-
-  tags          = azurerm_resource_group.this.tags
-}
-
 module "kvs_sas_token_datamig_charges" {
   source        = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/key-vault-secret?ref=7.0.0"
 
   name          = "datamig-charges"
   value         = var.sas_token_datamig_charges
-  key_vault_id  = module.kv_shared.id
-
-  tags          = azurerm_resource_group.this.tags
-}
-
-module "kvs_sas_token_datamig_metering_point" {
-  source        = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/key-vault-secret?ref=7.0.0"
-
-  name          = "datamig-metering-point"
-  value         = var.sas_token_datamig_metering_point
   key_vault_id  = module.kv_shared.id
 
   tags          = azurerm_resource_group.this.tags
@@ -51,11 +31,31 @@ module "kvs_sas_token_datamig_market_roles" {
   tags          = azurerm_resource_group.this.tags
 }
 
+module "kvs_sas_token_datamig_metering_point" {
+  source        = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/key-vault-secret?ref=7.0.0"
+
+  name          = "datamig-metering-point"
+  value         = var.sas_token_datamig_metering_point
+  key_vault_id  = module.kv_shared.id
+
+  tags          = azurerm_resource_group.this.tags
+}
+
 module "kvs_sas_token_datamig_time_series" {
   source        = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/key-vault-secret?ref=7.0.0"
 
   name          = "datamig-time-series"
   value         = var.sas_token_datamig_time_series
+  key_vault_id  = module.kv_shared.id
+
+  tags          = azurerm_resource_group.this.tags
+}
+
+module "kvs_sas_token_datamig_wholesale" {
+  source        = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/key-vault-secret?ref=7.0.0"
+
+  name          = "datamig-wholesale"
+  value         = var.sas_token_datamig_wholesale
   key_vault_id  = module.kv_shared.id
 
   tags          = azurerm_resource_group.this.tags
--- build/infrastructure/env/p-001/variables-additional.tf
+++ build/infrastructure/env/p-001/variables-additional.tf
@@ -11,19 +11,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-variable sas_token_datamig_wholesale {
-  type        = string
-  description = "The SAS token for the storage account containing migrated DataHub 2 data dedicated to the Wholesale domain"
-}
-
 variable sas_token_datamig_charges {
   type        = string
   description = "The SAS token for the storage account containing migrated DataHub 2 data dedicated to the Charges domain"
-}
-
-variable sas_token_datamig_metering_point {
-  type        = string
-  description = "The SAS token for the storage account containing migrated DataHub 2 data dedicated to the Metering point domain"
 }
 
 variable sas_token_datamig_market_roles {
@@ -31,7 +21,17 @@ variable sas_token_datamig_market_roles {
   description = "The SAS token for the storage account containing migrated DataHub 2 data dedicated to the Market roles domain"
 }
 
+variable sas_token_datamig_metering_point {
+  type        = string
+  description = "The SAS token for the storage account containing migrated DataHub 2 data dedicated to the Metering point domain"
+}
+
 variable sas_token_datamig_time_series {
   type        = string
   description = "The SAS token for the storage account containing migrated DataHub 2 data dedicated to the Time series domain"
+}
+
+variable sas_token_datamig_wholesale {
+  type        = string
+  description = "The SAS token for the storage account containing migrated DataHub 2 data dedicated to the Wholesale domain"
 }
