cb2fd607bd181fde07ae345bdfee5a1f55298de2
djorgensendk <32477395+djorgensendk@users.noreply.github.com>
2022-08-03 12:13:41 +0200

    Use OIDC with TF (#217)
    * use oidc
    
    * remove arm client secret variable
    
    * readd arm_client_id as it is used
    
    * variables not allowed in backend azurerm
    
    * add use_oidc = true to tf providers
    
    * try to comment out az login in script
    
    * remove arm_client_id and arm_client_secret vars
    
    * remove commented elements and write extra comment

--- build/infrastructure/main/scripts/generate-pat-token.sh
+++ build/infrastructure/main/scripts/generate-pat-token.sh
@@ -3,10 +3,9 @@
 # Bash strict mode, stop on any error
 set -e
 
-eval "$(jq -r '@sh "DATABRICKS_WORKSPACE_RESOURCE_ID=\(.DATABRICKS_WORKSPACE_RESOURCE_ID) ARM_CLIENT_ID=\(.ARM_CLIENT_ID) ARM_CLIENT_SECRET=\(.ARM_CLIENT_SECRET) ARM_TENANT_ID=\(.ARM_TENANT_ID) DATABRICKS_ENDPOINT=\(.DATABRICKS_ENDPOINT)"')"
+eval "$(jq -r '@sh "DATABRICKS_WORKSPACE_RESOURCE_ID=\(.DATABRICKS_WORKSPACE_RESOURCE_ID) DATABRICKS_ENDPOINT=\(.DATABRICKS_ENDPOINT)"')"
 
-# Login
-az login --service-principal -u "$ARM_CLIENT_ID" -p "$ARM_CLIENT_SECRET" -t "$ARM_TENANT_ID" --output none
+# Must be run on a machine that is already logged in to Azure
 
 # Get a token for the global Databricks application.
 # The resource name is fixed and never changes.
